a9142fd044c3f5df10c5b5e0e1887cea2b2a5027
Zabazabz <zabazaba780@gmail.com>
2025-10-03 20:39:56 +1000

    Добавил Procfile для Railway

--- bot.py
+++ bot.py
@@ -1,18 +1,14 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
-
-# 🔑 сюда вставь свой токен, который тебе дал BotFather
-TOKEN = "8016036296:AAH9amLMhzhZXx2J_MGdwHe6riHRjgk9Dz0"
-
+# 🔑 Теперь токен берётся из переменной окружения
+TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
 @dp.message()
 async def echo_handler(message: types.Message):
     await message.answer(f"Ты написал: {message.text}")
-
 async def main():
     await dp.start_polling(bot)
-
 if __name__ == "__main__":
     asyncio.run(main())
